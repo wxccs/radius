88bcfff9f1fdc8fcd72e134611186546531e9a33
Daniel Wu <wxc@wxccs.org>
2026-06-24 15:30:30 +0800

    chore: update copyright holder from wxccs to Daniel Wu
    Replace the author name in the SPDX MIT copyright notice across all
    source files. The module path (github.com/wxccs/radius) and import
    paths are intentionally left unchanged to avoid breaking the build.

--- protocol/authenticator_test.go
+++ protocol/authenticator_test.go
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 //
-// Copyright (c) 2026 wxccs
+// Copyright (c) 2026 Daniel Wu
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
